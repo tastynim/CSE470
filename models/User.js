43bd3a65946301ea185bace48b8f1ae45d7822c5
araniannesha <arani.annesha@g.bracu.ac.bd>
2026-02-27 17:32:19 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // MISSING FIELD ADDED!
+    password: { type: String, required: true }, 
     role: { type: String, default: 'customer' },
-    isRural: { type: Boolean, default: false }, // Added from your register body
-    location: { type: String },                 // Added from your register body
-    skills: [{ type: String }]                  // Added from your register body
+    isRural: { type: Boolean, default: false }, 
+    location: { type: String },                
+    skills: [{ type: String }]                  
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);;
+
