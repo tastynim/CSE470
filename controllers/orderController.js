1d63ea8c9a31d613b1f5feec24d1b2cebf6ed456
araniannesha <arani.annesha@g.bracu.ac.bd>
2026-02-27 17:26:01 +0600

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -65,4 +65,15 @@ const createOrder = async (req, res) => {
     }
 };
 
-module.exports = { createOrder };
+// 3. Get all orders
+const getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find();
+        res.status(200).json(orders);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to fetch orders' });
+    }
+};
+
+module.exports = { createOrder, getOrders };
