c146a7a08fe5bc604d3698172e2f979ebe00866f
araniannesha <arani.annesha@g.bracu.ac.bd>
2026-02-27 17:59:31 +0600

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder } = require('../controllers/orderController');
+const { createOrder, getOrders } = require('../controllers/orderController');
 
-// This creates the POST endpoint
+// GET all orders
+router.get('/', getOrders);
+
+// POST create new order
 router.post('/create', createOrder);
 
 module.exports = router;
