4f4fa996ce5c6c3e92fe783f8ef1b484b34d8d65
araniannesha <arani.annesha@g.bracu.ac.bd>
2026-03-18 15:48:04 +0600

    Add files via upload

---
+++ models/Message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    receiver: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    text: { 
+        type: String, 
+        required: true 
+    },
+    isRead: { 
+        type: Boolean, 
+        default: false 
+    }
+}, { timestamps: true }); // This automatically adds 'createdAt' and 'updatedAt' times!
+
+module.exports = mongoose.model('Message', messageSchema);
